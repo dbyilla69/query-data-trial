50f841cf5239e261437e8b19e11b58612738be29
dbyilla69 <doranyilla@gmail.com>
2021-03-17 12:38:55 -0500

    refactor the player details page

--- src/Pages/playerDetails.js
+++ src/Pages/playerDetails.js
@@ -1,29 +1,15 @@
 import AccordionPlayer from '../components/Players/accordionPlayer';
-import Avatar from '@material-ui/core/Avatar';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
 import CircleLoader from 'react-spinners/CircleLoader';
 import { Fragment } from 'react';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableRow from '@material-ui/core/TableRow';
-import Typography from '@material-ui/core/Typography';
-import { createData } from '../util';
 import { makeStyles } from '@material-ui/core/styles';
 import { useParams } from 'react-router-dom';
 import usePlayerDetails from '../hooks/usePlayerDetails';
+import PlayerCard from '../components/Players/playerCard';
+import PlayerInfo from '../components/Players/playerInfo';
+import MorePlayerInfo from '../components/Players/morePlayerInfo';
 
-const useStyles = makeStyles((theme) => ({
-  root: { minWidth: 400, marginLeft: theme.spacing(4) },
-  media: {
-    height: 440
-  },
+const useStyles = makeStyles(() => ({
   backgroundImage: {
     width: '100%',
     height: '1200px',
@@ -31,9 +17,6 @@ const useStyles = makeStyles((theme) => ({
     position: 'absolute',
     zIndex: '-1',
     opacity: '.4'
-  },
-  playerDemographicTable: {
-    width: '20em'
   }
 }));
 
@@ -49,25 +32,8 @@ const PlayerDetails = () => {
   const playerStat = data.playerStatsTotals[0].player;
   const stats = data.playerStatsTotals[0].stats;
   const teamLogo = data.references.teamReferences[0].officialLogoImageSrc;
-
   const secondaryTeamColor =
     data.references.teamReferences[0].teamColoursHex[1];
-
-  const playersInfo = [
-    createData('Position', playerStat.primaryPosition),
-    createData('Age', playerStat.age),
-    createData('Height', playerStat.height),
-    createData('Weight', playerStat.weight),
-    createData('Games Played', stats.gamesPlayed)
-  ];
-
-  const morePlayerInfo = [
-    createData('Jersey', playerStat.jerseyNumber),
-    createData('Status', playerStat.currentRosterStatus),
-    createData('Born', playerStat.birthDate),
-    createData('College', playerStat.college),
-    createData('Games Started', stats.miscellaneous.gamesStarted)
-  ];
 
   return (
     <Fragment>
@@ -79,57 +45,9 @@ const PlayerDetails = () => {
         }}
       />
       <Grid container spacing={4}>
-        <Card className={classes.root}>
-          <CardHeader
-            avatar={<Avatar src={teamLogo} aria-label='recipe' />}
-            title={
-              <Typography variant='h4' align='left'>
-                {`${playerStat.firstName} ${playerStat.lastName}`}
-              </Typography>
-            }
-          />
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image={playerStat.officialImageSrc}
-              title='Contemplative Reptile'
-            />
-          </CardActionArea>
-        </Card>
-        <Grid item>
-          <TableContainer
-            className={classes.playerDemographicTable}
-            component={Paper}
-          >
-            <Table size='small' aria-label='simple table'>
-              <TableBody>
-                {playersInfo.map((p) => (
-                  <TableRow key={p.name}>
-                    <TableCell>{p.name}</TableCell>
-                    <TableCell>{p.info}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
-        <Grid item>
-          <TableContainer
-            className={classes.playerDemographicTable}
-            component={Paper}
-          >
-            <Table size='small' aria-label='simple'>
-              <TableBody>
-                {morePlayerInfo.map((p) => (
-                  <TableRow key={p.name}>
-                    <TableCell component='th'>{p.name}</TableCell>
-                    <TableCell component='th'>{p.info}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Grid>
+        <PlayerCard playerStat={playerStat} teamLogo={teamLogo} />
+        <PlayerInfo playerStat={playerStat} stats={stats} />
+        <MorePlayerInfo playerStat={playerStat} stats={stats} />
       </Grid>
       <AccordionPlayer
         stats={stats}
