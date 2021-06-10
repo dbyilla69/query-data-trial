164d43af0bdf617a8d8daf005a79cee13fcd404c
dbyilla69 <doranyilla@gmail.com>
2021-06-10 16:23:34 -0500

    change to the correct api key

--- src/util.js
+++ src/util.js
@@ -2,7 +2,7 @@ export const createData = (name, info) => {
   return { name, info };
 };
 
-const username = '5f7e91ad-20ed-4f10-88d2-f0ea7d';
+const username = 'be66e55f-c4cd-4f6a-a662-9c7183';
 const password = 'MYSPORTSFEEDS';
 export const api = (team) =>
   `https://api.mysportsfeeds.com/v2.1/pull/nfl/2019-2020-regular/player_stats_totals.json?team=${team}&limit=20`;
